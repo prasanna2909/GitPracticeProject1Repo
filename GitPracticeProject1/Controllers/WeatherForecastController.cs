d84948a6a678b29f5ebbf0d9a363ecbed8f9eba7
prasanna2909 <prasannarudra4@gmail.com>
2025-08-06 13:35:41 +0530

    Update WeatherForecastController.cs
    Changes1

--- GitPracticeProject1/Controllers/WeatherForecastController.cs
+++ GitPracticeProject1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitPracticeProject1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" ,"Adding"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" ,"Adding" , "Deleting"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
